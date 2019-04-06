271dac996bc06e2eef860b8731ca4970b58ae604
Sean OLoughlin <27134812+SeanoNET@users.noreply.github.com>
2019-04-06 21:25:48 +0800

    Fixed css issue in prod Added search engine ignore index Changed default route

--- RoomTempDashboard/Startup.cs
+++ RoomTempDashboard/Startup.cs
@@ -64,7 +64,7 @@ namespace RoomTempDashboard
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Dashboard}/{id?}");
             });
         }
     }
