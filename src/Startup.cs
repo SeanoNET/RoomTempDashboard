3a66a71915d761645187e34547f4947b42944676
Sean O <27134812+SeanoNET@users.noreply.github.com>
2020-02-04 19:58:47 +0800

    Removed deprecated net core 2.2 methods Added environment variables config

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using RoomTempDashboard.Hubs;
-using Microsoft.EntityFrameworkCore;
 using RoomTempDashboard.Models;
+using System.IO;
 
 namespace RoomTempDashboard
 {
@@ -19,7 +16,11 @@ namespace RoomTempDashboard
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddEnvironmentVariables()
+                        .Build(); 
         }
 
         public IConfiguration Configuration { get; }
@@ -35,13 +36,13 @@ namespace RoomTempDashboard
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddSignalR();
             services.AddDbContext<HelloIotdbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("HelloIotDatabase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
